5eab8aaae6216430809782689915f116af72859a
mayurgudi <37340166+mayurgudi@users.noreply.github.com>
2020-09-15 19:12:12 +0530

    Add files via upload

---
+++ HospitalDAL/Class1.cs
@@ -0,0 +1,24 @@
+﻿using HospitalDLL;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HospitalDAL
+{
+    public class HospitalDB : DbContext
+    {
+        public HospitalDB(DbContextOptions<HospitalDB> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Patient>().ToTable("Patient");
+            modelBuilder.Entity<Problem>().ToTable("Problem");
+            modelBuilder.Entity<Treatment>().ToTable("Treatment");
+        }
+
+        public DbSet<Patient> patients { get; set; }
+    }
+}
---
+++ HospitalDAL/Migrations/20200825105151_v1.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalDAL.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patient",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patient", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Problem",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Patientid = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Problem", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Problem_Patient_Patientid",
+                        column: x => x.Patientid,
+                        principalTable: "Patient",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Treatment",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Dosage = table.Column<string>(nullable: true),
+                    Problemid = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treatment", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Treatment_Problem_Problemid",
+                        column: x => x.Problemid,
+                        principalTable: "Problem",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Problem_Patientid",
+                table: "Problem",
+                column: "Patientid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treatment_Problemid",
+                table: "Treatment",
+                column: "Problemid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Treatment");
+
+            migrationBuilder.DropTable(
+                name: "Problem");
+
+            migrationBuilder.DropTable(
+                name: "Patient");
+        }
+    }
+}
